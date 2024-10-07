e2c625d032525ccde059532f8120a883738e6cb9
[Samridhi Gupta] <samridhigupta1944@gmail.com>
2024-10-07 20:33:35 +0530

    E-commerce

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0038A8",
+        secondary: "#0038A8",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
